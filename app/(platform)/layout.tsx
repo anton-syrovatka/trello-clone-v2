9bcc463aee0d397368d654952f9cdae26c63e2f7
Anton Syrovatka <syrovatka.anton@gmail.com>
2024-01-14 16:20:47 +0100

    Boalds list: tooltip and popover

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,8 +1,14 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import React, { PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
+import { Toaster } from "sonner";
 
 function PlatformLayout({ children }: PropsWithChildren) {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <ClerkProvider>
+      <Toaster />
+      {children}
+    </ClerkProvider>
+  );
 }
 
 export default PlatformLayout;
