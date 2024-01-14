210c19faff6ed0c54cd1538ca7fbac342c8f5686
Anton Syrovatka <syrovatka.anton@gmail.com>
2024-01-14 14:14:10 +0100

    boards create-delete | prisma | form actions

---
+++ lib/db.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+// global is excluded from hot-reload
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+// prevents from multiple prisma initializations in dev mode during hot-reload
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
